20289091d51569420a8e85238e2c71367aa78234
Insik Cho <insik.cho@alaskaair.com>
2020-02-15 00:08:33 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const fetch = require('node-fetch');
+const { parse } = require('node-html-parser');
+const { convertOnClickUrl } = require('./utilities/urlConverter');
+
+const domain = 'https://scholar.google.com';
+const seedPath = `/citations?view_op=view_org&hl=en&org=11816294095661060495`;
+const outFileName = './outfiles/ucberkeley.csv';
+const interval = 1500;
+
+const retrieve10Page = (outFileName, path, counter, maxCount, userCounter) => {
+    counter = counter || 0;
+    maxCount = maxCount || 10000;
+    userCounter = userCounter || 0;
+
+    if (counter === 0) {
+        resetFile(outFileName);    
+    }
+    console.log(counter, path);
+    fetch(`${domain}${path}`)
+    .then(res => res.text())
+    .then(html => {
+        const root = parse(html);
+        const buttonNext = root.querySelector('.gs_btnPR');
+        const users = root.querySelectorAll('.gsc_1usr');
+
+        users.forEach(user => {
+            const name = user.querySelector('.gs_ai_name a');
+            const affiliate = user.querySelector('.gs_ai_aff');
+            const emailDomain = user.querySelector('.gs_ai_eml').text.replace('Verified email at ', '');
+            const keywords = user.querySelectorAll('.gs_ai_int .gs_ai_one_int').map(kw => kw.text).join('/');
+            appendToFile(outFileName, `${userCounter}, ${name.text}, ${affiliate.text}, ${emailDomain}, ${keywords}\n`)
+            ++userCounter;
+        });
+
+        const pathNext = convertOnClickUrl(buttonNext.rawAttrs.split(' ')[1]);
+
+        if (!buttonNext.rawAttrs.includes('disabled')) {
+            setTimeout(() =>retrieve10Page(outFileName, pathNext, ++counter, maxCount, userCounter), interval);
+        }
+    });
+};
+
+function appendToFile(filename, content) {
+    try {
+        fs.appendFileSync(filename, content);
+    } catch(err) {
+        console.error(err);
+    }
+}
+
+function resetFile(filename) {
+    try {
+        fs.writeFileSync(filename, '');
+    } catch(err) {
+        console.error(err);
+    }
+}
+
+retrieve10Page(outFileName, seedPath, 0, 0);
---
+++ utilities/urlConverter.js
@@ -0,0 +1,11 @@
+// <button type="button" onclick="window.location='/citations?view_op\x3dview_org\x26hl\x3den\x26oe\x3dASCII\x26org\x3d11816294095661060495\x26after_author\x3d4GEAAHC0_f8J\x26astart\x3d10'" aria-label="Next" class="gs_btnPR gs_in_ib gs_btn_half gs_btn_lsb gs_btn_srt gsc_pgn_pnx"><span class="gs_wr"><span class="gs_ico"></span><span class="gs_lbl"></span></span></button>
+function convertOnClickUrl(onClickUrl) {
+    return onClickUrl.replace(/onclick="window\.location='/g,'')
+            .replace(/\\x3d/g, '=')
+            .replace(/\\x26/g, '&')
+            .replace(/'"$/g, '');
+}
+
+module.exports = {
+    convertOnClickUrl
+};
