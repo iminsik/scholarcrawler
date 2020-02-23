b6b4f816533e2ba6f749909d0fc34f52ccaf12c8
Insik Cho <insik.cho@alaskaair.com>
2020-02-22 23:18:24 -0800

    formatted userCounter.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { parse } = require('node-html-parser');
 const { convertOnClickUrl, getRandomArbitrary } = require('./utilities/urlConverter');
 
 const MIN = 15, MAX = 20;
-const index = 8;
+const index = 9;
 const userCounter = 0;
 const domain = 'https://scholar.google.com';
 
@@ -36,7 +36,7 @@ const retrieve10Page = async (outFileName, path, univOfCounter, univOfMaxCount, 
     }
 
     // TODO: how to handle retry in fetching a list?
-    console.log(univOfCounter, orgCodeFiles[univOfCounter].name, `${userCounter}`.padStart(4, ''), path);
+    console.log(univOfCounter, orgCodeFiles[univOfCounter].name, `${userCounter}`.padStart(4, ' '), path);
     const responseProfiles = await fetch(`${domain}${path}`);
     const html = await responseProfiles.text();
 
